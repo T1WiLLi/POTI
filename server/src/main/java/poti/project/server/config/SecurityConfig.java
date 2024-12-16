82984fff63bcdcbb7e9dc3cdcd5d79df0ee0a6b8
William Beaudin <williambeaudun@gmail.com>
2024-12-16 12:47:51 -0500

    feat(Security): implement XSS and CSRF security protocol.

--- server/src/main/java/poti/project/server/config/SecurityConfig.java
+++ server/src/main/java/poti/project/server/config/SecurityConfig.java
@@ -3,13 +3,16 @@ package poti.project.server.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -23,7 +26,15 @@ public class SecurityConfig {
                             HttpServletRequest request = context.getRequest();
                             return new AuthorizationDecision(isRequestFromServer(request));
                         })
-                        .anyRequest().denyAll());
+                        .anyRequest().denyAll())
+                .headers(headers -> headers
+                        .frameOptions(Customizer.withDefaults())
+                        .xssProtection(xss -> xss.disable())
+                        .httpStrictTransportSecurity(hsts -> hsts
+                                .includeSubDomains(true)
+                                .maxAgeInSeconds(31536000))
+                        .contentSecurityPolicy(csp -> csp
+                                .policyDirectives("default-src 'self'; script-src 'self'")));
         return http.build();
     }
 
